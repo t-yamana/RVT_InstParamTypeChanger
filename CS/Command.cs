a8cc5b3ce6550a01f33cde867adece68692ec6d3
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-12-18 15:42:18 +0900

    left changes

--- CS/Command.cs
+++ CS/Command.cs
@@ -135,11 +135,11 @@ namespace Revit.SDK.Samples.ParameterUtils.CS
               // using ActiveDocument.GetElement(the element id) to 
               // retrieve the element from the active document
               Autodesk.Revit.DB.ElementId elemId = param.AsElementId();
-              Element elem = app.ActiveUIDocument.Document.GetElement(elemId);
+              Element relateElm = app.ActiveUIDocument.Document.GetElement(elemId);
 
               // if there is an element then display its name, 
               // otherwise display the fact that it is not set
-              sb.Append(elem != null ? elem.Name : "Not set");
+              sb.Append(relateElm != null ? relateElm.Name : "Not set");
               break;
             case Autodesk.Revit.DB.StorageType.Integer:
               // append the type and value
@@ -156,7 +156,73 @@ namespace Revit.SDK.Samples.ParameterUtils.CS
             default:
               break;
           }
+          // add the completed line to the string list
+          parameterItems.Add(sb.ToString());
+        }
 
+        foreach (Parameter param in elem.Parameters)
+        {
+          if (param == null) continue;
+
+          // We will make a string that has the following format,
+          // name type value
+          // create a StringBuilder object to store the string of one parameter
+          // using the character '\t' to delimit parameter name, type and value 
+          StringBuilder sb = new StringBuilder();
+
+          // the name of the parameter can be found from its definition.
+          var name = param.Definition.Name;
+          sb.AppendFormat("{0}\t", name);
+
+          if (insOrType.ContainsKey(param.Id))
+          {
+            sb.AppendFormat("{0}\t", insOrType[param.Id] ? "Inst" : "Type");
+          }
+          else
+          {
+            sb.AppendFormat("{0}\t", "-");
+          }
+
+          // Revit parameters can be one of 5 different internal storage types:
+          // double, int, string, Autodesk.Revit.DB.ElementId and None. 
+          // if it is double then use AsDouble to get the double value
+          // then int AsInteger, string AsString, None AsStringValue.
+          // Switch based on the storage type
+          switch (param.StorageType)
+          {
+            case Autodesk.Revit.DB.StorageType.Double:
+              // append the type and value
+              sb.AppendFormat("double\t{0}", param.AsDouble());
+              break;
+            case Autodesk.Revit.DB.StorageType.ElementId:
+              // for element ids, we will try and retrieve the element from the 
+              // document if it can be found we will display its name.
+              sb.Append("Element\t");
+
+              // using ActiveDocument.GetElement(the element id) to 
+              // retrieve the element from the active document
+              Autodesk.Revit.DB.ElementId elemId = param.AsElementId();
+              Element relateElm = app.ActiveUIDocument.Document.GetElement(elemId);
+
+              // if there is an element then display its name, 
+              // otherwise display the fact that it is not set
+              sb.Append(relateElm != null ? relateElm.Name : "Not set");
+              break;
+            case Autodesk.Revit.DB.StorageType.Integer:
+              // append the type and value
+              sb.AppendFormat("int\t{0}", param.AsInteger());
+              break;
+            case Autodesk.Revit.DB.StorageType.String:
+              // append the type and value
+              sb.AppendFormat("string\t{0}", param.AsString());
+              break;
+            case Autodesk.Revit.DB.StorageType.None:
+              // append the type and value
+              sb.AppendFormat("none\t{0}", param.AsValueString());
+              break;
+            default:
+              break;
+          }
           // add the completed line to the string list
           parameterItems.Add(sb.ToString());
         }
--- CS/PropertiesForm.Designer.cs
+++ CS/PropertiesForm.Designer.cs
@@ -51,52 +51,52 @@ namespace Revit.SDK.Samples.ParameterUtils.CS
         /// </summary>
         private void InitializeComponent()
         {
-            this.okButton = new System.Windows.Forms.Button();
-            this.propertyListView = new System.Windows.Forms.ListView();
-            this.SuspendLayout();
-            // 
-            // okButton
-            // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(386, 315);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 0;
-            this.okButton.Text = "&OK";
-            this.okButton.UseVisualStyleBackColor = true;
-            // 
-            // propertyListView
-            // 
-            this.propertyListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.propertyListView.FullRowSelect = true;
-            this.propertyListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.propertyListView.HideSelection = false;
-            this.propertyListView.Location = new System.Drawing.Point(12, 12);
-            this.propertyListView.MultiSelect = false;
-            this.propertyListView.Name = "propertyListView";
-            this.propertyListView.Size = new System.Drawing.Size(449, 297);
-            this.propertyListView.TabIndex = 2;
-            this.propertyListView.UseCompatibleStateImageBehavior = false;
-            this.propertyListView.View = System.Windows.Forms.View.Details;
-            // 
-            // PropertiesForm
-            // 
-            this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.okButton;
-            this.ClientSize = new System.Drawing.Size(473, 350);
-            this.Controls.Add(this.propertyListView);
-            this.Controls.Add(this.okButton);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "PropertiesForm";
-            this.ShowInTaskbar = false;
-            this.Text = "Properties";
-            this.ResumeLayout(false);
+      this.okButton = new System.Windows.Forms.Button();
+      this.propertyListView = new System.Windows.Forms.ListView();
+      this.SuspendLayout();
+      // 
+      // okButton
+      // 
+      this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.okButton.Location = new System.Drawing.Point(386, 636);
+      this.okButton.Name = "okButton";
+      this.okButton.Size = new System.Drawing.Size(75, 21);
+      this.okButton.TabIndex = 0;
+      this.okButton.Text = "&OK";
+      this.okButton.UseVisualStyleBackColor = true;
+      // 
+      // propertyListView
+      // 
+      this.propertyListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.propertyListView.FullRowSelect = true;
+      this.propertyListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+      this.propertyListView.HideSelection = false;
+      this.propertyListView.Location = new System.Drawing.Point(12, 11);
+      this.propertyListView.Name = "propertyListView";
+      this.propertyListView.Size = new System.Drawing.Size(449, 619);
+      this.propertyListView.TabIndex = 2;
+      this.propertyListView.UseCompatibleStateImageBehavior = false;
+      this.propertyListView.View = System.Windows.Forms.View.Details;
+      // 
+      // PropertiesForm
+      // 
+      this.AcceptButton = this.okButton;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.okButton;
+      this.ClientSize = new System.Drawing.Size(473, 668);
+      this.Controls.Add(this.propertyListView);
+      this.Controls.Add(this.okButton);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "PropertiesForm";
+      this.ShowInTaskbar = false;
+      this.Text = "Properties";
+      this.Load += new System.EventHandler(this.PropertiesForm_Load);
+      this.ResumeLayout(false);
 
         }
 
